72000c4bfc113aa618d0a21d7d674fdca94a4238
Naman Rai <namanrai306@gmail.com>
2023-01-25 01:20:40 -0500

    added admin page and route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,6 +4,8 @@ import Merch from "./Merch";
 import Footer from "./Footer";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import AdminPage from "./Admin";
+import SignUp from "./Signup"
 import { dark_Theme, light_Theme } from "./themes";
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
@@ -18,6 +20,8 @@ function App() {
           <Route path="/" element={<Merch />} />
           <Route path="/csshop" element={<Merch />} />
           <Route path="/product" element={<Checkout />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
 
         <div className='footer-section'>
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Button, Dialog, DialogContent, Link, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import "../styles/LoginSignup.css"
+import { useNavigate } from "react-router-dom"
 
 
 
 
 export default function Login({ trigger, onClick }) {
+  const navigate = useNavigate();
+
   return (
     
     <>
@@ -34,12 +37,12 @@ export default function Login({ trigger, onClick }) {
                     <TextField color="secondary" margin='normal' className='register-text' label="Email Address" variant="outlined"  required/>
                     <TextField color="secondary" margin='normal' className='register-text' label="Password" variant="outlined"  type='password'  required/>
                   </div>
-                  <div className='register-button'>
-                    <Button  type='submit' color="primary" variant='contained' >Login</Button>
+                  <div className='register-button' >
+                    <Button onClick={ ()=> navigate('./admin')} type='submit' color="primary" variant='contained' >Login</Button>
                   </div>
 
-                  <Typography className='register-link'> Do you have an account ?
-                    <Link href='#'>
+                  <Typography className='register-link' sx={{margin:3,fontSize: "1.2rem"}}> Do you have an account ?
+                    <Link onClick={ ()=> navigate('./signup')} sx={{margin:1, color: "white", fontSize: "1.2rem", fontWeight: "700" }} className="signup-style">
                        Sign up
                     </Link>
                   </Typography> 

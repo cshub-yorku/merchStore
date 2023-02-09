6b6311ba04565cd53b3f299d792ad3bafd761e9f
Naman Rai <namanrai306@gmail.com>
2023-02-08 22:16:58 -0500

    fixed signup and login components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Dialog, DialogContent, Link, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import "../styles/LoginSignup.css"
 import { useNavigate } from "react-router-dom"
+import Signup from './Signup';
 
 
 
 
 export default function Login({ trigger, onClick }) {
   const navigate = useNavigate();
+
+  const [switchSignup, setSwtichSignup] = useState(false);
+
+  const changeToFalse = () => {
+    setSwtichSignup(false);
+  }
 
   return (
     
@@ -21,13 +28,21 @@ export default function Login({ trigger, onClick }) {
             PaperProps={{
                 sx: {
                 bgcolor: "#1F1E3A",
-                width: "30%",
-                height: "60%",
+                width: "40%",
+                height: "80%",
                 borderRadius: "8px",
                 },
             }}
             >
-            <DialogContent
+            
+            <div className='outerContainer'>
+                {switchSignup ? (
+                  <div>
+                    <Signup changeToFalse = {changeToFalse} />
+                  </div>
+              
+              
+            ):(<DialogContent
                 classes={{ root: { m: 0, p: 0 } }}
                 sx={{ "&.MuiDialogContent-root": { m: 0, p: 0 } }}
             >
@@ -42,7 +57,7 @@ export default function Login({ trigger, onClick }) {
                   </div>
 
                   <Typography className='register-link' sx={{margin:3,fontSize: "1.2rem"}}> Do you have an account ?
-                    <Link onClick={ ()=> navigate('./signup')} sx={{margin:1, color: "white", fontSize: "1.2rem", fontWeight: "700" }} className="signup-style">
+                    <Link onClick={ ()=> setSwtichSignup(true)} sx={{margin:1, color: "white", fontSize: "1.4rem", fontWeight: "700" }} className="signup-style">
                        Sign up
                     </Link>
                   </Typography> 
@@ -50,7 +65,9 @@ export default function Login({ trigger, onClick }) {
                   
 
               </div>
-            </DialogContent>
+            </DialogContent>)}
+            </div>
+            
         </Dialog>
     </>
   )

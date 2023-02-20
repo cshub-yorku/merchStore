14252a65dd6ab238cdb3403e8482928e1494c0f6
Naman Rai <namanrai306@gmail.com>
2023-02-20 13:09:46 -0500

    added admin component

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import React from 'react'
 import "../styles/Admin.css";
 
@@ -5,7 +6,31 @@ import "../styles/Admin.css";
 export default function Admin() {
   return (
     <>
-    Admin page
+      <Box sx={{width: '80%',
+       height: "100%",
+       marginTop: '5vw',
+       backgroundColor: "#121212",
+       marginLeft: "10vw",
+       marginRight: "auto",
+       display: "flex",
+       justifyContent: "center",
+       alignItems: "center"}}>
+
+          {/* <Box
+              sx={{
+                width: "15vw",
+                height: "10vh",
+                backgroundColor: 'primary.dark',
+                '&:hover': {
+                  backgroundColor: 'white';
+                },
+              }}
+            >
+
+
+            </Box> */}
+            
+      </Box>
     </>
   )
 }

d4f1848ffd549bfbf05db85d0a68a06b32292c34
George <cradplay@gmail.com>
2023-03-20 01:49:07 -0400

    cart object added

---
+++ frontend/src/controllers/cart.js
@@ -0,0 +1,22 @@
+export var cart = {
+    cart: new Map(),
+
+    add: function (product) {
+        let amount = cart.cart.get(product.id)
+        cart.cart.set(product.id, amount ? amount + 1 : 1);
+    },
+
+    remove: (product) => {
+        let amount = cart.cart.get(product.id)
+        if (amount != 1)
+            cart.cart.set(product.id, amount - 1);
+        else
+            cart.cart.delete(product.id)
+            
+
+    },
+
+    getAll: () => {
+        return cart.cart;
+    },
+};

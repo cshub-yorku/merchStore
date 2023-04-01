751e0c265fe55b8558a08c60389592d7367771be
George <cradplay@gmail.com>
2023-04-01 02:08:21 -0400

    removed testing notification

--- frontend/src/components/Merch.js
+++ frontend/src/components/Merch.js
@@ -85,10 +85,10 @@ export default function Merch() {
             </Box>
           </Box>
 
-          <Button onClick={() => { setProductNotification(true) }}>Hi</Button>
+          {/* <Button onClick={() => { setProductNotification(true) }}>Hi</Button>
           <ProductNotification open={productNotification} setOpen={setProductNotification} product={testProduct}></ProductNotification>
 
-          <TextField sx={field_white} label="example"></TextField>
+          <TextField sx={field_white} label="example"></TextField> */}
 
           {/* <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
             <Typography>Sort</Typography>

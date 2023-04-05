d27caef7281bdecc293a8199e63d9bc681e9fd8b
George <cradplay@gmail.com>
2023-04-04 23:55:14 -0400

    reset backend changes

--- backend/src/main/java/com/MerchStore/backend/ResourceControllers/ProductController.java
+++ backend/src/main/java/com/MerchStore/backend/ResourceControllers/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/v1/products")
 public class ProductController {

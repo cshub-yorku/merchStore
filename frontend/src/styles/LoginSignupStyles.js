af35fb8c651bd733864b2057a7b85d6847ace0bc
Ricky Tran <rickie.tran7@gmail.com>
2023-04-09 14:53:45 -0400

    converted login css styles to js

---
+++ frontend/src/styles/LoginSignupStyles.js
@@ -0,0 +1,89 @@
+export const marginPaddingZero = {
+    m: 0, 
+    p: 0 
+}
+
+export const registerText = {
+    backgroundColor: 'white',
+    borderRadius: '8px'
+}
+
+export const signupText = {
+    width: "80%"
+}
+
+export const register = {
+    m: '15%',
+    mt: '10%'
+}
+
+export const registerFields = {
+    mt: '20%',
+    display: 'flex',
+    flexDirection: 'column'
+}
+
+export const registerButton = {
+    borderRadius: '5px', 
+    backgroundColor: '#624F82', 
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    mt: '8%', 
+    py: '0.8vh', 
+    px: '6vw', 
+    fontWeight: '700', 
+    fontSize: '1.3rem'
+}
+
+export const registerLink = {
+    fontSize: '0.5rem'
+}
+
+export const registerSignFields = {
+    mt: '4%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+}
+
+export const registerSign = {
+    m: '15%',
+    mt: '4%',
+    mb: '5%',
+}
+
+export const signupStyle = {
+    color: 'white',
+    textDecoration: 'none'
+}
+
+export const dialogPaperStyle = {
+    bgcolor: "#1F1E3A",
+    width: "40%",
+    height: "80%",
+    borderRadius: "8px",
+}
+
+export const forgotPasswordStyle = {
+    color: "white", 
+    fontSize: "1rem", 
+    fontWeight: "500" 
+}
+
+export const forgotPasswordLink = { 
+    m: 3, 
+    fontSize: "1.2rem" }
+
+export const haveAccount = { 
+    m: 3, 
+    fontSize: "1.2rem" 
+}
+
+export const signUpSwitch = {
+    m: 1,
+    color: "white",
+    fontSize: "1.4rem",
+    fontWeight: "700",
+    textDecoration: 'none',
+}
